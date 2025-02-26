d466f1da637a9ad6344f13b36d6ddd1b7676390c
Oleg Namozov <oleg8494@yandex.ru>
2025-02-26 16:54:44 +0300

    New

--- src/main/java/ru/oleg/Main.java
+++ src/main/java/ru/oleg/Main.java
@@ -1,5 +1,6 @@
 package ru.oleg;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,8 +8,10 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello qa.guru!");
-        System.out.printf("Hello qa!");
-        System.out.printf("Hello !");
+        System.out.printf("Hello!");
+        System.out.printf("Hello@");
+        System.out.printf("Hello#");
+        System.out.printf("Hello$");
 
         for (int i = 1; i <= 5; i++) {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
