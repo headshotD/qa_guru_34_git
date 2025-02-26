713b74cc9f690519f6aadd2d347be0af765fd3c6
Oleg Namozov <oleg8494@yandex.ru>
2025-02-26 16:49:11 +0300

    NewMaster

--- src/main/java/ru/oleg/Main.java
+++ src/main/java/ru/oleg/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa.guru!");
+        System.out.printf("Hello qa.guru123!");
 
         for (int i = 1; i <= 5; i++) {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
